76125037475392f976539c6f4c7a35974b1bdb22
LupinaIgor <igorlupina222@gmail.com>
2025-01-09 12:53:44 +0200

    Three methods added

--- first_app/cars/urls.py
+++ first_app/cars/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import create_car
+from .views import create_car, update_car, delete_car, get_all_cars
 
 urlpatterns = [
     path('create', create_car),
     # path('<int:pk>/', get_single_car),
+    path('update/<int:pk>/', update_car),  # Добавлено для обновления
+    path('delete/<int:pk>/', delete_car),  # Новый маршрут для удаления
+    path('all/', get_all_cars),  # Новый маршрут для получения всех объектов
 ]
--- first_app/cars/views.py
+++ first_app/cars/views.py
@@ -16,9 +16,41 @@ def create_car(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-#
-# @api_view(['GET'])
-# def get_single_car(request, pk):
-#     car = Car.objects.get(id=pk)
-#     serializer = CarSerializer(car)
-#     return Response(serializer.data)
+
+@api_view(['GET'])
+def get_single_car(request, pk):
+    car = Car.objects.get(id=pk)
+    serializer = CarSerializer(car)
+    return Response(serializer.data)
+
+# добавление
+@api_view(['PUT'])
+def update_car(request, pk):
+    try:
+        car = Car.objects.get(id=pk)
+    except Car.DoesNotExist:
+        return Response({"error": "Car not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = CarSerializer(car, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# видалення
+@api_view(['DELETE'])
+def delete_car(request, pk):
+    try:
+        car = Car.objects.get(id=pk)
+    except Car.DoesNotExist:
+        return Response({"error": "Car not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    car.delete()
+    return Response({"message": f"Car with id {pk} was deleted successfully."}, status=status.HTTP_200_OK)
+
+# читання усіх
+@api_view(['GET'])
+def get_all_cars(request):
+    cars = Car.objects.all()  # Получаем все объекты Car
+    serializer = CarSerializer(cars, many=True)  # Сериализуем список объектов
+    return Response(serializer.data, status=status.HTTP_200_OK)
